8190b5e2a50ab09d8347c0dc623566f9ae4906ca
Mr. Laugh <143761740+LOLDude2427@users.noreply.github.com>
2025-11-29 13:11:15 -0500

    App Data
    - Fix app title
    - Add app icon

---
+++ RippingToolsGUI/ColConvPage.xaml.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RippingToolsGUI
+{
+    /// <summary>
+    /// Interaction logic for ColConvPage.xaml
+    /// </summary>
+    public partial class ColConvPage : Page
+    {
+        public ColConvPage()
+        {
+            InitializeComponent();
+            btnConvCol.IsEnabled = false;
+        }
+
+        string ColDir = "None";
+        private void btnSelectCol_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = ("Collision file|*.phy.hkx");
+
+            if (ofd.ShowDialog() == true)
+            {
+                btnConvCol.IsEnabled = true;
+
+                string fullPath = ofd.FileName;
+                ColDir = fullPath.Substring(0, fullPath.LastIndexOf('\\'));
+
+                foreach (String item in Directory.EnumerateFiles(ColDir, "*.phy.hkx"))
+                {
+                    listBoxColView.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnConvCol_Click(object sender, RoutedEventArgs e)
+        {
+
+            if(ColVersion == "None")
+            {
+                MessageBox.Show("No version selected.", "Not all parameters filled.");
+            }
+
+            else if(ColVersion == "ColGenerations")
+            {
+                var sourceDirPath = System.IO.Path.Combine(Environment.CurrentDirectory, "Resources/ColConv/Generations");
+                var sourceDirInfo = new DirectoryInfo(sourceDirPath);
+
+                var targetDirPath = ColDir;
+                var targetDirInfo = new DirectoryInfo(targetDirPath);
+
+                CopyFiles(sourceDirInfo, targetDirInfo);
+
+                string batLoc = string.Format($@"{ColDir}");   //this is where the .bat is 
+                Process proc = new Process();
+                proc.StartInfo.WorkingDirectory = batLoc;
+                proc.StartInfo.FileName = "BatchConvert.bat";
+                proc.StartInfo.CreateNoWindow = false;
+                proc.Start();
+
+                proc.WaitForExit();
+
+                listBoxColView.Items.Clear();
+                System.IO.File.Delete($@"{ColDir}/BatchConvert.bat");
+                System.IO.File.Delete($@"{ColDir}/collision2fbx.exe");
+
+                MessageBox.Show("Converted *.phy.hkx to *.fbx.");
+            }
+            else if(ColVersion == "ColLostWorld")
+            {
+                var sourceDirPath = System.IO.Path.Combine(Environment.CurrentDirectory, "Resources/ColConv/LostWorld");
+                var sourceDirInfo = new DirectoryInfo(sourceDirPath);
+
+                var targetDirPath = ColDir;
+                var targetDirInfo = new DirectoryInfo(targetDirPath);
+
+                CopyFiles(sourceDirInfo, targetDirInfo);
+
+                string batLoc = string.Format($@"{ColDir}");   //this is where the .bat is 
+                Process proc = new Process();
+                proc.StartInfo.WorkingDirectory = batLoc;
+                proc.StartInfo.FileName = "BatchConvert.bat";
+                proc.StartInfo.CreateNoWindow = false;
+                proc.Start();
+
+                proc.WaitForExit();
+
+                listBoxColView.Items.Clear();
+                System.IO.File.Delete($@"{ColDir}/BatchConvert.bat");
+                System.IO.File.Delete($@"{ColDir}/TagTools.exe");
+                System.IO.File.Delete($@"{ColDir}/TagTools.VIR");
+                System.IO.File.Delete($@"{ColDir}/TypeDatabase.xml");
+
+                MessageBox.Show("Converted *.phy.hkx to *.fbx.");
+            }
+        }
+
+        private void CopyFiles(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            foreach (var file in source.GetFiles())
+            {
+                file.CopyTo(System.IO.Path.Combine(target.FullName, file.Name), true);
+            }
+        }
+
+        string ColVersion = "None";
+        private void comboBoxColVersion_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBoxColVersion == null) return;
+            var combo = (ComboBox)sender;
+            var item = (ComboBoxItem)combo.SelectedItem;
+            ColVersion = item.Content.ToString();
+
+            if (ColVersion == "System.Windows.Controls.Label: Generations")
+            {
+                ColVersion = "ColGenerations";
+            }
+            else if (ColVersion == "System.Windows.Controls.Label: Lost World")
+            {
+                ColVersion = "ColLostWorld";
+            }
+        }
+    }
+}
---
+++ RippingToolsGUI/DDSConvPage.xaml.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RippingToolsGUI
+{
+    /// <summary>
+    /// Interaction logic for DDSConvPage.xaml
+    /// </summary>
+    public partial class DDSConvPage : Page
+    {
+        public DDSConvPage()
+        {
+            InitializeComponent();
+            btnConvDDS.IsEnabled = false;
+            btnFixForcesDDS.IsEnabled = false;
+            btnFixStreaming.IsEnabled = false;
+        }
+
+        string StreamingDir;
+        string DDSDir;
+        private void btnSelectDDS_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = ("DDS Texture|*.dds");
+
+            if (ofd.ShowDialog()==true)
+            {
+                btnConvDDS.IsEnabled = true;
+                btnFixForcesDDS.IsEnabled = true;
+                btnFixStreaming.IsEnabled = true;
+
+                string fullPath = ofd.FileName;
+                DDSDir = fullPath.Substring(0, fullPath.LastIndexOf('\\'));
+
+                foreach (String item in Directory.EnumerateFiles(DDSDir, "*.dds"))
+                {
+                    listBox.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnConvDDS_Click(object sender, RoutedEventArgs e)
+        {
+
+            var sourceDirPath = System.IO.Path.Combine(Environment.CurrentDirectory, "Resources/DDSConv");
+            var sourceDirInfo = new DirectoryInfo(sourceDirPath);
+
+            var targetDirPath = DDSDir;
+            var targetDirInfo = new DirectoryInfo(targetDirPath);
+
+            CopyFiles(sourceDirInfo, targetDirInfo);
+
+            string batLoc = string.Format($@"{DDSDir}");   //this is where the .bat is 
+            Process proc = new Process();
+            proc.StartInfo.WorkingDirectory = batLoc;
+            proc.StartInfo.FileName = "DDS2PNG.bat";
+            proc.StartInfo.CreateNoWindow = false;
+            proc.Start();
+
+            proc.WaitForExit();
+
+            listBox.Items.Clear();
+            System.IO.File.Delete($@"{DDSDir}/texconv.exe");
+            System.IO.File.Delete($@"{DDSDir}/DDS2PNG.bat");
+            System.IO.File.Delete($@"{DDSDir}/ForcesDDSFix.bat");
+
+            btnConvDDS.IsEnabled = false;
+            btnFixForcesDDS.IsEnabled = false;
+
+            MessageBox.Show("Converted all *.dds textures to *.png.");
+        }
+
+        private void CopyFiles(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            foreach(var file in source.GetFiles())
+            {
+                file.CopyTo(System.IO.Path.Combine(target.FullName, file.Name), true);
+            }
+        }
+
+        private void btnFixForcesDDS_Click(object sender, RoutedEventArgs e)
+        {
+            var sourceDirPath = System.IO.Path.Combine(Environment.CurrentDirectory, "Resources/DDSConv");
+            var sourceDirInfo = new DirectoryInfo(sourceDirPath);
+
+            var targetDirPath = DDSDir;
+            var targetDirInfo = new DirectoryInfo(targetDirPath);
+
+            CopyFiles(sourceDirInfo, targetDirInfo);
+
+            string batLoc = string.Format($@"{DDSDir}");   //this is where the .bat is 
+            Process proc = new Process();
+            proc.StartInfo.WorkingDirectory = batLoc;
+            proc.StartInfo.FileName = "ForcesDDSFix.bat";
+            proc.StartInfo.CreateNoWindow = false;
+            proc.Start();
+
+            proc.WaitForExit();
+
+            System.IO.File.Delete($@"{DDSDir}/texconv.exe");
+            System.IO.File.Delete($@"{DDSDir}/DDS2PNG.bat");
+            System.IO.File.Delete($@"{DDSDir}/ForcesDDSFix.bat");
+            btnFixForcesDDS.IsEnabled = false;
+
+            MessageBox.Show("Fixed all Forces *.dds textures.");
+        }
+
+        private void btnSelectStreamingPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = ("NTSP File|*.ntsp");
+
+            if (ofd.ShowDialog() == true)
+            {
+                string fullPath = ofd.FileName;
+                StreamingDir = fullPath.Substring(0, fullPath.LastIndexOf('\\'));
+
+                StreamWriter sw = new StreamWriter("Resources/Streaming/needle_texture_streaming_path.txt");
+                sw.WriteLine(StreamingDir);
+                sw.Close();
+            }
+        }
+        private void btnFixStreaming_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(System.IO.Path.Combine(Environment.CurrentDirectory, "Resources/Streaming/NeedleTextureStreamingPackage.exe"), DDSDir);
+            MessageBox.Show("Converting. If it didn't work, select streaming path ('game_name'/image/x64/raw/texture_streaming)");
+        }
+    }
+}
